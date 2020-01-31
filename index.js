a10e21da0972ec56edb1842457e8c15414a26a39
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-02-01 00:53:58 +0530

    updated index.js
    .

--- index.js
+++ index.js
@@ -12,20 +12,31 @@ const readFilePro = (file) => {
    });
 };
 
-readFilePro(`${__dirname}/dog.txt`).then((data) => {
-   console.log(`Breed: ${data}`);
-
-   superagent
-      .get(`https://dog.ceo/api/breed/${data}/images/random`)
-      .then((res) => {
-         console.log(res.body.message);
-
-         fs.writeFile('dog-ig.txt', res.body.message, (err) => {
-            console.log(`Random dog image save to file!`);
-         });
-      })
-
-      .catch((err) => {
-         console.log(err.message);
+const writeFilePro = (file, data) => {
+   return new Promise((reject, resolve) => {
+      fs.writeFile(file, data, (err) => {
+         if (err) {
+            console.log('Could not write a file!');
+         }
+         resolve(`success`);
       });
-});
+   });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+   .then((data) => {
+      console.log(`Breed: ${data}`);
+
+      return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+   })
+   .then((res) => {
+      console.log(res.body.message);
+
+      fs.writeFile('dog-ig.txt', res.body.message, (err) => {
+         console.log(`Random dog image save to file!`);
+      });
+   })
+
+   .catch((err) => {
+      console.log(err.message);
+   });
